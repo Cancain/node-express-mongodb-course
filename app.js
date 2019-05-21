6fb168e63ac2662fa1010e5a9b73bf345b121932
Cancain <eriksson.tomas@gmail.com>
2019-05-21 18:19:51 +0200

    completed lecture 11

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require("express");
 
 const app = express();
 
+//Index route
+app.get("/", (req, res) => {
+  res.send("Index");
+});
+
+//About route
+app.get("/about", (req, res) => {
+  res.send("About");
+});
 const port = 5000;
 
 app.listen(port, () => {
