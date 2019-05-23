37bf8a66ab8c756acc835271ebb8c8b4184c036f
Cancain <eriksson.tomas@gmail.com>
2019-05-23 13:57:29 +0200

    Completed lecture 24

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const methodOverride = require("method-override");
+const flash = require("connect-flash");
+const session = require("express-session");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
@@ -36,6 +38,25 @@ app.use(bodyParser.json());
 
 //Method override middleware
 app.use(methodOverride("_method"));
+
+//Express session middleware
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+app.use(flash());
+
+//Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
 
 app.set("view engine", "handlebars");
 //Index route
@@ -102,6 +123,7 @@ app.post("/ideas", (req, res) => {
       details: req.body.details
     };
     new Idea(newUser).save().then(idea => {
+      req.flash("success_msg", "Idea added");
       res.redirect("/ideas");
     });
   }
@@ -112,10 +134,12 @@ app.put("/ideas/:id", (req, res) => {
   Idea.findOne({
     _id: req.params.id
   }).then(idea => {
+    //New values
     idea.title = req.body.title;
     idea.details = req.body.details;
 
     idea.save().then(idea => {
+      req.flash("success_msg", "Idea updated");
       res.redirect("/ideas");
     });
   });
@@ -124,6 +148,7 @@ app.put("/ideas/:id", (req, res) => {
 //Delete idea
 app.delete("/ideas/:id", (req, res) => {
   Idea.remove({ _id: req.params.id }).then(() => {
+    req.flash("success_msg", "Idea deleted");
     res.redirect("/ideas");
   });
 });
