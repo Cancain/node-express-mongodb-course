6be84e978929e9c3109a2f31ab5e8239f17f0b95
Cancain <eriksson.tomas@gmail.com>
2019-06-01 15:02:32 +0200

    completed lecture 32

--- routes/users.js
+++ routes/users.js
@@ -87,4 +87,10 @@ router.post("/register", (req, res) => {
   }
 });
 
+router.get("/logout", (req, res) => {
+  req.logOut();
+  req.flash("success_msg", "You are now logged out");
+  res.redirect("/users/login");
+});
+
 module.exports = router;
